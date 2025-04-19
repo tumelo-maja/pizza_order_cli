90987031671233ee64f3f601d1603187a8c54038
tumelo-maja <mogale.ramsey@gmail.com>
2025-04-19 02:44:43 +0100

    Connect google sheets API

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from gspread_dataframe import get_as_dataframe
 
-print("Main is running")
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('fresh_leafy_greens')
+
+orders_sheet = SHEET.worksheet('orders')
+
+orders_df = get_as_dataframe(orders_sheet)
+print(orders_df)
+
+print("Hello")
